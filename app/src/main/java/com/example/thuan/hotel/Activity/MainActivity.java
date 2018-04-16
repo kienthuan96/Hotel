0f00762bf2f3e44ed4df585553b2aaec213cb37f
triplethuan96 <triplethuan96@gmail.com>
2018-04-16 21:59:55 +0700

    chia ra 3 thu muc

--- app/src/main/java/com/example/thuan/hotel/Activity/MainActivity.java
+++ app/src/main/java/com/example/thuan/hotel/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.thuan.hotel;
+package com.example.thuan.hotel.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.thuan.hotel.R;
 
 public class MainActivity extends AppCompatActivity {
 
