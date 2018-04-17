063e3c1f68077292369221f04fbd1f28c08a73f8
triplethuan96 <triplethuan96@gmail.com>
2018-04-17 10:06:23 +0700

    ket noi firebase

--- app/src/main/java/com/example/thuan/hotel/Activity/MainActivity.java
+++ app/src/main/java/com/example/thuan/hotel/Activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.thuan.hotel.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Chào");
     }
 }
