536688698773f2697c4bff1e3e1177aaf76238bf
Nguyen Vo Hoang Duy <nguyenvohoangduy@gmail.com>
2018-04-17 14:50:37 +0700

    Add package

---
+++ app/src/main/java/com/example/thuan/hotel/Activity/Activity/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.thuan.hotel.Activity.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.thuan.hotel.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
