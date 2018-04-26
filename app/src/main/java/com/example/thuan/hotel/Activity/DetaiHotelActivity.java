eed270d7784c19942d21b829d41291571471b952
Nguyen Vo Hoang Duy <nguyenvohoangduy@gmail.com>
2018-04-26 18:34:32 +0700

    Update favorite

--- app/src/main/java/com/example/thuan/hotel/Activity/DetaiHotelActivity.java
+++ app/src/main/java/com/example/thuan/hotel/Activity/DetaiHotelActivity.java
@@ -1,16 +1,24 @@
 package com.example.thuan.hotel.Activity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.thuan.hotel.Helper.Database;
 import com.example.thuan.hotel.Model.Hotel;
 import com.example.thuan.hotel.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,8 +28,13 @@ import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 public class DetaiHotelActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    final String DATABASE_NAME = "database.sqlite";
+    SQLiteDatabase databaseSQL;
     TabHost tabHost;
-    String id_hotel="-LAqUFdTyh6UXVtnASG9";
+    Intent intent = getIntent();
+    Bundle bundle = intent.getBundleExtra("goi");
+    String id_hotel = bundle.getString("id");
     TextView txtTenKS,txtDiaChiKS,txtGiaKS,txtSDTKS;
     ImageView img;
     DatabaseReference myRef;
@@ -55,7 +68,12 @@ public class DetaiHotelActivity extends AppCompatActivity {
 
         load();
         //Toast.makeText(DetaiHotelActivity.this,"Load",Toast.LENGTH_SHORT).show();
-
+        clickFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addEventFavorite();
+            }
+        });
     }
 
     private void load(){
@@ -90,5 +108,23 @@ public class DetaiHotelActivity extends AppCompatActivity {
         img=findViewById(R.id.imgHotel);
         ratingBar=findViewById(R.id.rbKS);
         clickFavorite=findViewById(R.id.clickFavorite);
+    }
+
+    private void addEventFavorite() {
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("user_id", user.getUid());
+        contentValues.put("hotel_id", id_hotel);
+
+        databaseSQL = Database.initDatabase(this, DATABASE_NAME);
+        Cursor cursor = databaseSQL.rawQuery("SELECT * FROM favorite where user_id = '" + user.getUid()  +"' AND hotel_id = '"+ id_hotel +"'",null);
+        if(cursor.getCount() != 0) {
+            Toast.makeText(DetaiHotelActivity.this, "Khách sạn này đã thêm vào danh sách yêu thích của bạn", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            databaseSQL.insert("favorite",null, contentValues);
+            Toast.makeText(DetaiHotelActivity.this, "Thêm vào danh sách yêu thích thành công", Toast.LENGTH_SHORT).show();
+        }
     }
 }
